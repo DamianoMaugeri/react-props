4fb5f89ff543e66f1f79c56d12c087a2e544c85a
DamianoMaugeri <damianomaugeri97@icloud.com>
2024-11-28 18:21:39 +0100

    [add] tag nelle card e tags nella section dedicata

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,43 @@ import Button from './Button'
 const placeHolderSrc = 'https://picsum.photos/1200/1000'
 
 
-export default function Card({ title = '', image = '', description = '', tags = [], published }) {
+let tag_css = ''
+
+export function setClass(el, classe) {
+    el = ''
+    switch (classe) {
+
+        case 'html':
+            el = 'html';
+            break;
+
+        case 'js':
+            el = 'js';
+            break;
+
+        case 'php':
+            el = 'php';
+            break;
+
+        case 'css':
+            el = 'css';
+            break;
+
+    }
+    return el
+
+}
+
+
+
+
+export function Card({ title = '', image = '', description = '', tags = [], }) {
+    console.log(tags)
+
+
+
+
+
     return (
         <div className={style.card}>
             <div className={style.card_header}>
@@ -17,6 +53,7 @@ export default function Card({ title = '', image = '', description = '', tags = 
                 <div className={style.title}>
                     <h3>{title}</h3>
                 </div>
+                {tags.length ? <div>{tags.map((tag, i) => <span key={i} className={setClass(tag_css, tag)}>{tag}</span>)}</div> : <div>Nessun tag </div>}
                 <p className={style.description}>
                     {description}
                 </p>
--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -1,8 +1,17 @@
 import { posts } from '../posts.jsx'
-import Card from './Card.jsx'
+import { Card, setClass } from './Card.jsx'
 import style from './MainSection.module.css'
 
 
+
+
+const postsTags = posts.map(post => post.tags).flat()
+
+function filtraTagUnici(array) {
+    return array.filter((el, i) => array.indexOf(el) === i);
+}
+
+console.log(filtraTagUnici(postsTags))
 
 
 export default function MainSection() {
@@ -14,11 +23,20 @@ export default function MainSection() {
                         {posts.map((post) => (
                             post.published &&
                             <div key={post.id} className="col_4">
-                                <Card title={post.title} image={post.image} description={post.content} tags={post.tags} published={post.published} />
+                                <Card title={post.title} image={post.image} description={post.content} tags={post.tags} />
                             </div>
                         ))}
 
                     </div>
+                </div>
+            </section>
+            <section>
+                <div className="tags">
+                    {filtraTagUnici(postsTags).map((tag, i) => (
+                        <div className={setClass(tag, tag)} key={i}>{tag}</div>
+
+                    ))}
+
                 </div>
             </section>
         </main>
